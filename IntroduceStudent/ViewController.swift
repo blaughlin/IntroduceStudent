03dd270670397d5190855f2a25ff38de2c37101e
Bernard Laughlin <bernardlaughlin@gmail.com>
2023-12-20 18:05:02 -0900

    First commit of app.
    First commit of app.

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//   IntroduceStudent
+//
+//  Created by Bernard Laughlin on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var schoolNameTextField: UITextField!
+    @IBOutlet var yearSegmentControl: UISegmentedControl!
+    @IBOutlet var morePetsStepper: UIStepper!
+    @IBOutlet var morePetsSwitch: UISwitch!
+    @IBOutlet var numberOfPetsLabel: UILabel!
+    
+
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        // Lets us choose the title we have selected from the segmented control
+        // In our example that is whether it is first, second, third or forth
+        let year = yearSegmentControl.titleForSegment(at: yearSegmentControl.selectedSegmentIndex)
+
+        // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
+        let introduction = "My name is \(firstNameTextField.text!)\(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets. "
+        
+        // Creates the alert where we pass in our message, which is our introduction
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        // A way to dismiss the box once it pops up
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        // Passing this action to the alert controller so it can be dismissed
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
