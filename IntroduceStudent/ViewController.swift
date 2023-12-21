aec3e882181b39f2adbbe15fceb6bc273174658b
Bernard Laughlin <bernardlaughlin@gmail.com>
2023-12-20 18:49:19 -0900

    added random color button and hometown textfield

--- IntroduceStudent/ViewController.swift
+++ IntroduceStudent/ViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myView: UIView!
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var schoolNameTextField: UITextField!
     @IBOutlet var yearSegmentControl: UISegmentedControl!
     @IBOutlet var morePetsStepper: UIStepper!
     @IBOutlet var morePetsSwitch: UISwitch!
+    @IBOutlet var hometownTextField: UITextField!
     @IBOutlet var numberOfPetsLabel: UILabel!
     
 
@@ -22,6 +24,10 @@ class ViewController: UIViewController {
         numberOfPetsLabel.text = "\(Int(sender.value))"
     }
     
+    @IBAction func changeBackgroundColorDidTapped(_ sender: UIButton) {
+        let colorArray = [UIColor.systemRed, UIColor.systemBrown, UIColor.systemGray, UIColor.systemMint,UIColor.systemTeal, UIColor.systemBackground, UIColor.systemCyan, UIColor.systemPink, UIColor.systemPurple, UIColor.systemIndigo, UIColor.systemYellow, UIColor.systemOrange]
+        myView.backgroundColor = colorArray.randomElement()!
+    }
     
     @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
         // Lets us choose the title we have selected from the segmented control
@@ -29,7 +35,7 @@ class ViewController: UIViewController {
         let year = yearSegmentControl.titleForSegment(at: yearSegmentControl.selectedSegmentIndex)
 
         // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
-        let introduction = "My name is \(firstNameTextField.text!)\(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets. "
+        let introduction = "My name is \(firstNameTextField.text!)\(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). My hometown is \(hometownTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets. "
         
         // Creates the alert where we pass in our message, which is our introduction
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
